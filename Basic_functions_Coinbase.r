7ebcbab1af726d436558b2dbba2d6c464ebd55e2
Heiko Kulinna <Heikokulinna@gmx.de>
2020-09-29 16:13:38 +0200

    Data aggregation

--- Basic_functions_Coinbase.r
+++ Basic_functions_Coinbase.r
@@ -54,7 +54,8 @@ parse_response <- function(path, query = NULL) {
 public_candles <- function(product_id = "ETH-USD",
                            start = NULL,
                            end = NULL,
-                           granularity = NULL) {
+                           granularity = NULL,
+                           local_time = TRUE) {
   #case remediation
   product_id <- toupper(product_id)
   
@@ -78,28 +79,19 @@ public_candles <- function(product_id = "ETH-USD",
   
   names(content) <-
     c("time", "low", "high", "open", "close", "volume")
-  content$time <-
-    as.POSIXct(content$time, origin = "1970-01-01") #, tz = "GMT")
+  if (local_time == TRUE){
+    content$time <-
+      as.POSIXct(content$time, origin = "1970-01-01") #, tz = "GMT")
+  }
+  if (local_time != TRUE){
+    content$time <-
+      as.POSIXct(content$time, origin = "1970-01-01", tz = "GMT") 
+  }
   #
   content <- content[order(content$time, decreasing = TRUE),]
   #return----
   return(content)
 }
-
-
-
-####
-public_candles(product_id = "ETH-USD", granularity = 900)
-
-
-
-public_candles(product_id = "ETH-USD", granularity = 60)
-
-
-# Daten Abruf und aggregation
-first_data <- public_candles(product_id = "ETH-USD", granularity = 60)
-first_data[1:20,]
-
 
 
 aggregate_public_candles<-function(data, aggregation = 5){
@@ -138,15 +130,85 @@ aggregate_public_candles<-function(data, aggregation = 5){
 }
 
 
-aggregate_public_candles(data = first_data, aggregation = 15)
+
+save_data<-function(data, path){
+    #
+    
+    CSV_ETH <- list.files(path = path, pattern = paste0(deparse(substitute(data)),".csv") )
+    #CSV_ETH
+    
+    if (length(CSV_ETH) == 0){
+        # file does not exist yet, need to create it
+        your_path = paste0(path, "\\", deparse(substitute(data)),".csv")
+        write.csv(data, your_path, row.names = FALSE)
+    }
+    
+    # file does exist in folder and will be modified
+    if (length(CSV_ETH) != 0){
+        #
+        existing_data<-read.csv(file =  paste0(path, "\\", deparse(substitute(data)),".csv"), )
+        first.row = existing_data[1,]
+
+        to_check.zeit<-as.character(first.row$time)
+        data.zeit<-as.character(data$time)
+        index_similar = which(data.zeit == to_check.zeit)
+
+        # if no similarity add data to existing data
+        if (length(index_similar) == 0 ) {
+              write.table(data,
+              file = paste0(path, "\\", deparse(substitute(data)),".csv"), 
+              append = T,
+              sep=",",
+              row.names=F,
+              col.names=F)
+        }
+
+        # add new data rows to existing data file
+        if (length(index_similar) == 1) {
+              write.table(data[1:(index_similar-1),],
+              file = paste0(path, "\\", deparse(substitute(data)),".csv"), 
+              append = T,
+              sep=",",
+              row.names=F,
+              col.names=F)
+        }
+
+        #read, order, remove duplicated and save file again
+        existing_data<-read.csv(file =  paste0(path, "\\", deparse(substitute(data)),".csv"), )
+        existing_data <- existing_data[order(existing_data$time, decreasing = TRUE),]
+        existing_data <- existing_data[!duplicated(existing_data$time),]
+        #
+        your_path = paste0(path, "\\", deparse(substitute(data)),".csv")
+        write.csv(existing_data , your_path, row.names = FALSE)
+
+    }
+
+}
 
 
 
 
+# Daten Abruf und aggregation
+# 1 Min Data
+ETH_USD_1 <- public_candles(product_id = "ETH-USD", granularity = 60)
+
+# 5 Min aggregated Data
+ETH_USD_5 <- aggregate_public_candles(data = ETH_USD_1, aggregation = 5)
+
+#15 Min aggregated Data
+ETH_USD_15 <- public_candles(product_id = "ETH-USD", granularity = 900)
 
 
 
+### Test save 1 und 5 Min Data
+path = "C:\\Users\\Heiko\\Visual.Studio\\R_Trading_Coinbase_Pro"
 
+
+save_data(data = ETH_USD_1 , path = path)
+
+save_data(data = ETH_USD_5 , path = path)
+
+save_data(data = ETH_USD_15 , path = path)
 
 
 
